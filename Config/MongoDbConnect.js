7c7585b46d595ee4b449c8537635c6ab3db10067
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-03-10 11:15:23 +0400

    change mongo db connect

--- Config/MongoDbConnect.js
+++ Config/MongoDbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ConnectDB = async () => {
   try {
-    await mongoose.connect(process.env.URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       dbName: process.env.DB_NAME,
     });
     console.log("MongoDB bağlantısı uğurla quruldu!");
