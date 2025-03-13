44456800f7b91d724940287455fe053ba5b4968b
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-03-13 21:40:06 +0400

    vercel

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const ConnectDB = require("./Config/MongoDbConnect");
-const userRoutes = require("./Routers/authRoute");
+// const userRoutes = require("./Routers/authRoute");
 const productRoutes = require("./Routers/ProductRoute");
 const projectRoutes = require("./Routers/ProjectRoute");
 const emailRoutes = require("./Routers/EmailRoute");
 const viewedRoutes = require("./Routers/RecentlyViewedRoute");
 const bodyParser = require("body-parser");
-
-const allowedOrigins = [
-  "https://furnite-ui.vercel.app",
-  "http://localhost:5173",
-];
 
 const app = express();
 
@@ -20,31 +15,12 @@ const app = express();
 app.use(express.json());
 // Buraya frontend URL-nı əlavə et
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://furnite-ui.vercel.app",
-      "http://localhost:5173",
-      "https://furniture-server-theta.vercel.app",
-    ];
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin); // Düzgün origin qaytarırıq
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 
 app.use(bodyParser.json());
 
 // Router-lər
-app.use("/api/users", userRoutes);
+// app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/project", projectRoutes);
 app.use("/api/viewed", viewedRoutes);
