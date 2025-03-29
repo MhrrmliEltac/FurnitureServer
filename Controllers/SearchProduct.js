8d1a5ac89b64f73972b7b1dd7ed17d485f7e89b2
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-03-29 19:14:44 +0400

    change search controller

--- Controllers/SearchProduct.js
+++ Controllers/SearchProduct.js
@@ -1,12 +1,25 @@
 const Product = require("../Models/ProductModel");
 
 const searchProduct = async (req, res) => {
-  const product = await Product.find({ name: req.query.query });
+  try {
+    const query = req.query.query;
 
-  if (!product || product.length === 0)
-    return res.status(404).json({ message: "Product not found" });
+    if (!query) {
+      return res.status(400).json({ message: "Query boş ola bilməz" });
+    }
 
-  return res.status(200).json({ message: "Product find", product });
+    const product = await Product.find({
+      name: { $regex: query, $options: "i" },
+    });
+
+    if (product.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    return res.status(200).json({ message: "Product found", product });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error", error });
+  }
 };
 
 module.exports = searchProduct;
